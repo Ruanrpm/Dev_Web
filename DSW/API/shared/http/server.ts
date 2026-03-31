eaba5bacbe540ed9d13118f472358ec6312fa7c8
Ruan <ruan.pablo.martinz@gmail.com>
2026-03-31 11:33:17 -0300

    Add files via upload

---
+++ DSW/API/shared/http/server.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+import routes from '../routes';
+import { error } from 'node:console';
+import AppError from '../errors/AppError';
+import { AppDataSource } from '../typeorm/data-source';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.massage
+        });
+    }
+    return response.status(500).json({
+        status: 'error',
+        massage: 'Internal Server Error'
+    });
+})
+
+AppDataSource.initialize()
+.then(()=>{
+    console.log("Data Source Initialized!");
+    app.listen(3333, () => {
+    console.log('Server started on port 3333!');
+})
+})
+.catch((err)=>{
+    console.log("Error during Data Source Initialized", err);
+})
+
+console.log('...');
