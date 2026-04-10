c0f8767c8f9f214e603da7285f3f92a00820e378
Ruan <ruan.pablo.martinz@gmail.com>
2026-04-10 09:42:17 -0300

    Add files via upload

--- DSW/API/src/shared/routes/index.ts
+++ DSW/API/src/shared/routes/index.ts
@@ -1,6 +1,8 @@
 import {response, Router} from "express";
+import productRouter from "./product.routes";
 
 const routes = Router();
+routes.use('/products', productRouter)
 
 routes.get('/', (request, response) => {
     response.json({message: 'Hellow Dev!'});
--- DSW/API/src/shared/routes/product.routes.ts
+++ DSW/API/src/shared/routes/product.routes.ts
@@ -3,3 +3,45 @@ import ProductController from "src/modules/products/controllers/ProductControlle
 
 const productRouter = Router();
 const productController = new ProductController;
+
+productRouter.get('/', async (req, res, next)=>{
+    try{
+        await productController.index(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
+
+productRouter.get('/:id', async (req, res, next)=>{
+    try{
+        await productController.show(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
+
+productRouter.post('/', async (req, res, next)=>{
+    try{
+        await productController.create(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
+
+productRouter.put('/:id', async (req, res, next)=>{
+    try{
+        await productController.update(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
+
+productRouter.delete('/', async (req, res, next)=>{
+    try{
+        await productController.delete(req, res, next);
+    }catch(err){
+        next(err);
+    }
+});
+
+export default productRouter;
